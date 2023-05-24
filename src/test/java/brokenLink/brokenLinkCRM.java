7a9432c2604be28043eabefedcc06a0eec3eacb7
802072 <802072@W10AP-Nol5JVpWv.VNSNY.ORG>
2023-05-24 09:42:57 -0400

    Modified script for excel data input and for TestNG report.

--- src/test/java/brokenLink/brokenLinkCRM.java
+++ src/test/java/brokenLink/brokenLinkCRM.java
@@ -6,22 +6,20 @@ import java.io.IOException;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
-
-import org.apache.poi.ss.usermodel.Row;
-import org.apache.poi.ss.usermodel.Sheet;
-import org.apache.poi.ss.usermodel.Workbook;
-import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Reporter;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 import io.github.bonigarcia.wdm.WebDriverManager;
+import testBrokenLinkCRM.dataDriven;
 
 public class brokenLinkCRM {
 
@@ -32,12 +30,11 @@ public class brokenLinkCRM {
 	String myurl = "";
 	HttpURLConnection myhuc = null;
 	int responseCode = 200;
-	
-	String excelPath = "C:\\Users\\802072\\git\\brokenLinkTestCRM\\resources\\testData\\testData.xlsx";  
+
+	String excelPath = "C:\\Users\\802072\\git\\brokenLinkTestCRM\\src\\test\\resources\\testData\\testData.xlsx";
 	String sheetName = "loginInfo";
-	String username = "";
-	String password = "";
-	
+
+
 	@BeforeTest
 	public void setUp() {
 		WebDriverManager.chromedriver().setup();
@@ -47,30 +44,14 @@ public class brokenLinkCRM {
 
 		driver.get(myhomePage);
 
-		//Read Excel file for username and password
-		try {
-			FileInputStream fileInputStream = new FileInputStream(new File(excelPath));
-			Workbook workbook = new XSSFWorkbook(fileInputStream);
-			Sheet sheet = workbook.getSheet(sheetName);
-
-			Row row = sheet.getRow(1);
-			org.apache.poi.ss.usermodel.Cell usernameCell = row.getCell(0);
-			org.apache.poi.ss.usermodel.Cell passwordCell = row.getCell(1);
-
-			username = usernameCell.getStringCellValue();
-			password = passwordCell.getStringCellValue();
-			System.out.println("The logged in user is:"+ username);
-			
-			workbook.close();
-			fileInputStream.close();
-			
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
 	}
 
-	@Test (description= "log into the homepage")
-	public void login() {
+	@Test (description= "log in to homepage")
+	public void logIn() throws IOException {
+		dataDriven d=new dataDriven();
+		ArrayList data=d.getData("User1","loginInfo");
+		String username= (String) data.get(1);
+		String password= (String) data.get(2);
 
 		//enter username
 		WebElement uname = driver.findElement(By.xpath("//input[@id='input-25']"));
@@ -95,25 +76,32 @@ public class brokenLinkCRM {
 		} catch (InterruptedException e) {
 			e.printStackTrace();
 		}
+
+		Reporter.log("Login is successful with user name : "+ data.get(1));
 	}
 
-	@Test (description= "Check for broken links")
+	@Test (description= "Check for broken links in homepage")
 	public void testBrokenLinks() {
-		List<WebElement> mylinks = driver.findElements(By.xpath("//a"));
+		ArrayList ls = new ArrayList();
 
+		List<WebElement> mylinks = driver.findElements(By.xpath("//a"));
+		Reporter.log("The total hyperlinks count in page is :"+mylinks.size());
 		Iterator<WebElement> myit = mylinks.iterator();
 		while (myit.hasNext()) {
 
 			myurl = myit.next().getAttribute("href");
-			System.out.println("The link is :"+myurl);
-			System.out.println(myurl);
+			//System.out.println("The link is :"+myurl);
+
+
 			if (myurl == null || myurl.isEmpty()) {
-				System.out.println("Empty URL or an Unconfigured URL");
+				//System.out.println("Empty URL or an Unconfigured URL");
+				Reporter.log("Empty URL or an Unconfigured URL");
 				continue;
 			}
 
 			if (!myurl.contains("https://vnshealth")) {
-				System.out.println("This URL is from another domain");
+				//System.out.println("This URL is from another domain");
+				Reporter.log(myurl+ "This URL is from another domain");
 				continue;
 			}
 
@@ -122,27 +110,32 @@ public class brokenLinkCRM {
 				myhuc.setRequestMethod("HEAD");
 				myhuc.connect();
 				responseCode = myhuc.getResponseCode();
-				System.out.println("The response code is:"+responseCode);
-				if (responseCode >= 400) {
-					System.out.println(myurl + " This link is broken");
-				} else {
-					System.out.println(myurl + " This link is valid");
-				}
 
+				if (responseCode >= 400) {
+					//System.out.println(myurl + " This link is broken");
+					ls.add(responseCode);
+
+					Reporter.log(myurl + " This link is broken");
+				} else {
+					//System.out.println(myurl + " This link is valid");
+					//Reporter.log(myurl + " This link is valid");
+				}
+				//System.out.println("The response code is:"+responseCode);
+				//Reporter.log("The response code is:"+responseCode);
 			} catch (MalformedURLException ex) {
 				ex.printStackTrace();
 			} catch (IOException ex) {
 				ex.printStackTrace();
 			}
 		}
+		Reporter.log("The broken links count is: "+ls.size());
 	}
+
 
 	@AfterTest
 	public void tearUp() {
 		driver.close();
+
 	}
 
-
 }
-
-
